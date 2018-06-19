9ba2a08313d9422d21a5ce45bbad925954e38072
Linus6 <sunilmathad6@gmail.com>
2018-06-19 21:23:24 +0530

    port updated

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = 3000;        // set our port
+var port = process.env.PORT || 8080;        // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
@@ -21,7 +21,6 @@ var router = express.Router();              // get an instance of the express Ro
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 app.get('/', function(req, res) {
-	console.log("get call")
     res.json({ message: 'hooray! welcome to lin api!' });   
 });
 
